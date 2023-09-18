f9e14e964cc7bce0530db69d4e197ba44e9ec7c1
Atshilla Fatih Ibrahim <shilla.ibra@gmail.com>
2023-09-18 10:50:19 +0700

    materi basic terakhir

--- main.go
+++ main.go
@@ -1,16 +1,1 @@
 package main
-
-import "fmt"
-
-func main() {
-
-	numbers := []int64{1, 2, 3, 4, 5}
-	for i, v := range numbers {
-		fmt.Printf("Aku Dapet %d di index %d\n", v, i)
-	}
-	// atau
-	for i := range numbers {
-		fmt.Printf("Aku Dapet %d di index %d\n", numbers[i], i)
-	}
-
-}
